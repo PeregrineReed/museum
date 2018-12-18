7f700537f0f7e86686f1e8f7b7fcecca8c0d6963
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-18 09:08:10 -0700

    Created lib and testing files with exhibit and patron classes

---
+++ test/patron_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+
+class PatronTest < Minitest::Test
+
+  def setup
+    @bob = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @bob
+  end
+
+end
