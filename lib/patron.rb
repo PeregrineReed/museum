fc70d60db01a40b1e68be1afe87653e231cd0ff1
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-18 09:17:32 -0700

    Added cost and spending money instance variables/attr_readers

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,9 +1,11 @@
 class Patron
 
-  attr_reader :name
+  attr_reader :name,
+              :spending_money
 
   def initialize(name, spending_money)
     @name = name
+    @spending_money = spending_money
   end
 
 end
